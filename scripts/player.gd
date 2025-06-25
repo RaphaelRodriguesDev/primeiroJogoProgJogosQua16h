4a16cef1c9578196c9693087c9a49159004074f7
Raphael Rodrigues <professor.dev.raphael@gmail.com>
2025-06-25 18:08:16 -0300

    Criação do player

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,5 +5,10 @@ var move_speed = 400
 var gravity = 1200
 var jump_force = -720
 
-func _physics_process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	velocity.y += gravity * delta
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	velocity.x = move_speed * move_direction
+	
+	move_and_slide(velocity)
